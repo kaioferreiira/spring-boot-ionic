1806e5761ee591084c2738b404e764b537d35010
Kaio Ferreira <kaioferreiracosta@hotmail.com>
2018-10-13 17:46:32 -0300

    Produto e associação ManyToMany

--- src/main/java/com/kaioferreira/cursomc/CursomcApplication.java
+++ src/main/java/com/kaioferreira/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.kaioferreira.cursomc;
 
 import com.kaioferreira.cursomc.domain.Categoria;
+import com.kaioferreira.cursomc.domain.Produto;
 import com.kaioferreira.cursomc.repository.CategoriaRepository;
+import com.kaioferreira.cursomc.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,18 +12,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+public class CursomcApplication implements CommandLineRunner {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 =  new Categoria(null, "Informática");
-		Categoria cat2 =  new Categoria(null, "Escritório");
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+
+    }
 }
--- src/main/java/com/kaioferreira/cursomc/domain/Categoria.java
+++ src/main/java/com/kaioferreira/cursomc/domain/Categoria.java
@@ -1,19 +1,23 @@
 package com.kaioferreira.cursomc.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Categoria implements Serializable {
+
+    private  static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @ManyToMany(mappedBy="categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria() {
     }
@@ -37,6 +41,14 @@ public class Categoria implements Serializable {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
