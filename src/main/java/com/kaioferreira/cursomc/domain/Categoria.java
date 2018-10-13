59f4346f10a23bea5819c1adf6eb08342c68cfa2
Kaio Ferreira <kaioferreiracosta@hotmail.com>
2018-10-13 16:07:32 -0300

    Banco de dados H2 e criando entidade de teste

--- src/main/java/com/kaioferreira/cursomc/domain/Categoria.java
+++ src/main/java/com/kaioferreira/cursomc/domain/Categoria.java
@@ -1,11 +1,18 @@
 package com.kaioferreira.cursomc.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Categoria implements Serializable {
 
-    private  Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String nome;
 
     public Categoria() {
